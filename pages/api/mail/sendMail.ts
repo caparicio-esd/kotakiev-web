b0099ca939acaa1b1906518b9b997673531e8f89
caparicio-esd <caparicio@esdmadrid.es>
2021-08-04 17:57:44 +0200

    server setup

---
+++ pages/api/mail/sendMail.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+/**
+ * Express-like http controller...
+ */
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method == "POST") {
+        res.send({"hola": "joooorl..."})
+    } else {
+        res.send({"hola": "jarl..."})
+    }
+}
